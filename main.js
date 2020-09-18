11de92f90d3a8ea42b7815802fbb852f95e614de
edufrojo <edufrojo@gmail.com>
2020-09-18 12:09:47 +0200

    feat: nueva lista de nacimiento

---
+++ main.js
@@ -0,0 +1,25 @@
+var listArt = articulos;
+
+var contenedor = document.getElementById("lista-articulos");
+
+for (let i = 0; i < listArt.length; i++) {
+  if (listArt[i].Reserva == 0) {
+    contenedor.innerHTML +=
+      '<div class="articulo"><img class="articulo__imagen" src="' +
+      listArt[i].Imagen +
+      '" alt="" /><h3 class="articulo__titulo">' +
+      listArt[i].Nombre +
+      '</h3><div class="articulo__precio">' +
+      listArt[i].Precio +
+      "</div></div>";
+  } else {
+    contenedor.innerHTML +=
+      '<div class="articulo"><img class="articulo__imagen" src="' +
+      listArt[i].Imagen +
+      '" alt="" /><h3 class="articulo__titulo">' +
+      listArt[i].Nombre +
+      '</h3><div class="articulo__precio">' +
+      listArt[i].Precio +
+      '</div><div class="articulo__reserva">RESERVADO</div>';
+  }
+}
